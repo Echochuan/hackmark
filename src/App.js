64aa5ea31b37bfd7857a5bd1aa5ba1fd77f2e4fa
Echochuan <912297261@qq.com>
2021-04-07 15:05:10 +0800

    初始化页面布局

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React from 'react';
 import './App.css';
+import List from './components/List/index'
+import { Layout, Menu } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
-function App() {
+const { Header, Content, Footer, Sider } = Layout;
 
+class App extends React.Component{
+render(){
+  return(
+  <Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          评分
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          <List/>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
+  )
+}
 }
 
 export default App;
