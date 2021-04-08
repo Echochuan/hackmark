0606cfcf0e96f1a52d4161055c571223403bfc9e
Echochuan <912297261@qq.com>
2021-04-08 23:17:49 +0800

    完善运营页面的跳转逻辑

--- src/components/Director/index.js
+++ src/components/Director/index.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react'
 import { Route } from 'react-router'
 import './index.css'
 
-import Groupchoose from "../GroupChoose/index";
 import markfordi from "../Mark/MarkForDi/index"
 
 export default class index extends Component {
     render() {
         return (
             <div>
-                <Groupchoose />
+                <div>
+                    <a href='/director/group_1'>
+                        <button className="eachButton" onClick = {this.Group_1}>第一组</button>
+                    </a>
+                    <a href='/director/group_2'>
+                        <button className="eachButton" onClick = {this.Group_2}>第二组</button>
+                    </a>
+                    <a href='/director/group_3'>
+                        <button className="eachButton" onClick = {this.Group_3}>第三组</button>
+                    </a>
+                    <a href='/director/group_4'>
+                        <button className="eachButton" onClick = {this.Group_4}>第四组</button>
+                    </a>
+                    <a href='/director/group_5'>
+                        <button className="eachButton" onClick = {this.Group_5}>第五组</button>
+                    </a>
+                </div>
                 <Route path='/director/Group_1' component={markfordi} />
                 <Route path='/director/Group_2' component={markfordi} />
                 <Route path='/director/Group_3' component={markfordi} />
--- src/components/Mark/MarkForDi/index.js
+++ src/components/Mark/MarkForDi/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
-import { Input } from 'antd';
+import { Input, Button, Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { Route, useHistory } from "react-router-dom";
 import './index.css'
 
+const { confirm } = Modal;
 
 export default class index extends Component {
+
+    showConfirm = () => {
+        confirm({
+          title: '请问你是否要提交？',
+          icon: <ExclamationCircleOutlined />,
+          content: '确定后无法修改，请多多检查',
+          onOk() {
+            console.log('Ok');
+          },
+          onCancel() {
+            console.log('Cancel');
+          },
+        });
+      }
+
     render() {
         return (
             <div className="boxForDi">
@@ -18,6 +36,7 @@ export default class index extends Component {
                     <Input className="eachEnter" addonBefore="Question" suffix="分"  defaultValue="" />
                     <Input className="eachEnter" addonBefore="Question" suffix="分"  defaultValue="" />
                 </div>
+                <Button className="YesBtn" size="large" onClick= { this.showConfirm }> 确认 </Button>
             </div>
         )
     }
