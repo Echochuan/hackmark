457dd0d38b02858814b17fb355d8bfeac0c6218d
Echochuan <912297261@qq.com>
2021-04-11 01:32:38 +0800

    对登陆页面优化

--- src/login.js
+++ src/login.js
@@ -3,7 +3,9 @@ import './login.css'
 import { Form, Input, Button } from "antd";
 
 class Login extends React.Component {
+
   render() {
+
     const layout = {
       labelCol: {
         span: 8
@@ -21,6 +23,15 @@ class Login extends React.Component {
     };
 
       const onFinish = values => {
+        //把values传给后端判断是否正确 如果正确 跳转到下一个页面，如果不正确 则提示错误
+        //也可以把username传过去，拿来password 判断password是不是相等
+        const id={username:'1',password:'1'}
+        if(id.username === values.username && id.password === values.password ) {
+          console.log("yes!");
+          //尚未解决的问题：函数式跳转
+        }else{
+          alert("信息错误");
+        }
         console.log("Success:", values);
       };
 
@@ -28,6 +39,13 @@ class Login extends React.Component {
         console.log("Failed:", errorInfo);
       };
 
+      const login = values => {
+        if(true) {
+          console.log("yes")
+        }else{
+          console.log("false")
+        }
+      }
 
     return (
         <div className="loginBox">
@@ -69,7 +87,7 @@ class Login extends React.Component {
         </Form.Item>
 
         <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" onClick={ login }>
             Submit
           </Button>
         </Form.Item>
---
+++ src/router/router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route} from 'react-router'
+
+
+class routes extends React.Component {
+    render(){
+        return(
+            <div>
+            <Route path='/home'></Route>
+        </div>
+        )
+    }
+}
+
+export default routes;
