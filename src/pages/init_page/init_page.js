ba87796277dedb0ab7da614f861a3bc2fdc5f43b
Echochuan <912297261@qq.com>
2021-04-11 18:26:21 +0800

    尝试更新key以改变路由

--- src/pages/init_page/init_page.js
+++ src/pages/init_page/init_page.js
@@ -1,31 +1,62 @@
 import React, { Component } from "react";
-import { Route } from 'react-router';
+import { Route } from "react-router";
 import { Layout, Menu } from "antd";
 
-import director from '../mark/markForDi/index'
+import director from "../mark/markForDi/index";
 
-import {
-  UserOutlined
-} from "@ant-design/icons";
+import { UserOutlined } from "@ant-design/icons";
 
-import createHashHistory from 'history/createHashHistory';
+import createHashHistory from "history/createHashHistory";
 const hashHistory = createHashHistory();
 
 const { Header, Content, Footer, Sider } = Layout;
-const {SubMenu} = Menu;
-
-
+const { SubMenu } = Menu;
 
 export default class init_page extends Component {
+  updata = value => {
+    console.log("updata start");
+    console.log(value);
+    if (value == 8 && value == 15) {
+      value = 1;
+      return value;
+    }
+    if (value == 9 && value == 16) {
+      value = 2;
+      return value;
+    }
+    if (value == 10 && value == 17) {
+      value = 3;
+      return value;
+    }
+    if (value == 11 && value == 18) {
+      value = 4;
+      return value;
+    }
+    if (value == 12 && value == 19) {
+      value = 5;
+      return value;
+    }
+    if (value == 13 && value == 20) {
+      value = 6;
+      return value;
+    }
+    if (value == 14 && value == 21) {
+      value = 7;
+      return value;
+    }
+    else return value;
+  };
 
-  Click = (e) => {
+  Click = e => {
     const position = e.keyPath[1];
-    const group = e.keyPath[0]
-    hashHistory.push('/init/'+ position + '/' + group)
-  }
+    const group = e.keyPath[0];
+    const groupid = this.updata(group);
+    console.log(groupid);
+    hashHistory.push("/init/" + position + "/" + groupid);
+  };
 
   render() {
-    console.log("hi,init")
+    console.log("hi,init");
     return (
       <div>
         <Layout>
@@ -40,122 +71,57 @@ export default class init_page extends Component {
             }}
           >
             <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}  onClick={this.Click}>
-            <SubMenu key="director" icon={<UserOutlined />} title="运营组">
-              <Menu.Item key="1">
-                第一组
-              </Menu.Item>
-              <Menu.Item key="2" >
-                第二组
-              </Menu.Item>
-              <Menu.Item key="3" >
-                第三组
-              </Menu.Item>
-              <Menu.Item key="4" >
-                第四组
-              </Menu.Item>
-              <Menu.Item key="5" >
-                第五组
-              </Menu.Item>
-              <Menu.Item key="6" >
-                第六组
-              </Menu.Item>
-              <Menu.Item key="7" >
-                第七组
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu key="product" icon={<UserOutlined />} title="产品组">
-              <Menu.Item key="8" >
-                第一组
-              </Menu.Item>
-              <Menu.Item key="9" >
-                第二组
-              </Menu.Item>
-              <Menu.Item key="10" >
-                第三组
-              </Menu.Item>
-              <Menu.Item key="11" >
-                第四组
-              </Menu.Item>
-              <Menu.Item key="12" >
-                第五组
-              </Menu.Item>
-              <Menu.Item key="13" >
-                第六组
-              </Menu.Item>
-              <Menu.Item key="14" >
-                第七组
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu key="design" icon={<UserOutlined />} title="设计组">
-              <Menu.Item key="15" >
-                第一组
-              </Menu.Item>
-              <Menu.Item key="16" >
-                第二组
-              </Menu.Item>
-              <Menu.Item key="17" >
-                第三组
-              </Menu.Item>
-              <Menu.Item key="18" >
-                第四组
-              </Menu.Item>
-              <Menu.Item key="19" >
-                第五组
-              </Menu.Item>
-              <Menu.Item key="20" >
-                第六组
-              </Menu.Item>
-              <Menu.Item key="21" >
-                第七组
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu key="front" icon={<UserOutlined />} title="前端组">
-              <Menu.Item key="22" >
-                第一组
-              </Menu.Item>
-              <Menu.Item key="23" >
-                第二组
-              </Menu.Item>
-              <Menu.Item key="24" >
-                第三组
-              </Menu.Item>
-              <Menu.Item key="25" >
-                第四组
-              </Menu.Item>
-              <Menu.Item key="26" >
-                第五组
-              </Menu.Item>
-              <Menu.Item key="27" >
-                第六组
-              </Menu.Item>
-              <Menu.Item key="28" >
-                第七组
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu key="back" icon={<UserOutlined />} title="后端组">
-              <Menu.Item key="29" >
-                第一组
-              </Menu.Item>
-              <Menu.Item key="30" >
-                第二组
-              </Menu.Item>
-              <Menu.Item key="31" >
-                第三组
-              </Menu.Item>
-              <Menu.Item key="32" >
-                第四组
-              </Menu.Item>
-              <Menu.Item key="33" >
-                第五组
-              </Menu.Item>
-              <Menu.Item key="34" >
-                第六组
-              </Menu.Item>
-              <Menu.Item key="35" >
-                第七组
-              </Menu.Item>
-            </SubMenu>
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={["4"]}
+              onClick={this.Click}
+            >
+              <SubMenu key="director" icon={<UserOutlined />} title="运营组">
+                <Menu.Item key="1">第一组</Menu.Item>
+                <Menu.Item key="2">第二组</Menu.Item>
+                <Menu.Item key="3">第三组</Menu.Item>
+                <Menu.Item key="4">第四组</Menu.Item>
+                <Menu.Item key="5">第五组</Menu.Item>
+                <Menu.Item key="6">第六组</Menu.Item>
+                <Menu.Item key="7">第七组</Menu.Item>
+              </SubMenu>
+              <SubMenu key="product" icon={<UserOutlined />} title="产品组">
+                <Menu.Item key="8">第一组</Menu.Item>
+                <Menu.Item key="9">第二组</Menu.Item>
+                <Menu.Item key="10">第三组</Menu.Item>
+                <Menu.Item key="11">第四组</Menu.Item>
+                <Menu.Item key="12">第五组</Menu.Item>
+                <Menu.Item key="13">第六组</Menu.Item>
+                <Menu.Item key="14">第七组</Menu.Item>
+              </SubMenu>
+              <SubMenu key="design" icon={<UserOutlined />} title="设计组">
+                <Menu.Item key="15">第一组</Menu.Item>
+                <Menu.Item key="16">第二组</Menu.Item>
+                <Menu.Item key="17">第三组</Menu.Item>
+                <Menu.Item key="18">第四组</Menu.Item>
+                <Menu.Item key="19">第五组</Menu.Item>
+                <Menu.Item key="20">第六组</Menu.Item>
+                <Menu.Item key="21">第七组</Menu.Item>
+              </SubMenu>
+              <SubMenu key="front" icon={<UserOutlined />} title="前端组">
+                <Menu.Item key="22">第一组</Menu.Item>
+                <Menu.Item key="23">第二组</Menu.Item>
+                <Menu.Item key="24">第三组</Menu.Item>
+                <Menu.Item key="25">第四组</Menu.Item>
+                <Menu.Item key="26">第五组</Menu.Item>
+                <Menu.Item key="27">第六组</Menu.Item>
+                <Menu.Item key="28">第七组</Menu.Item>
+              </SubMenu>
+              <SubMenu key="back" icon={<UserOutlined />} title="后端组">
+                <Menu.Item key="29">第一组</Menu.Item>
+                <Menu.Item key="30">第二组</Menu.Item>
+                <Menu.Item key="31">第三组</Menu.Item>
+                <Menu.Item key="32">第四组</Menu.Item>
+                <Menu.Item key="33">第五组</Menu.Item>
+                <Menu.Item key="34">第六组</Menu.Item>
+                <Menu.Item key="35">第七组</Menu.Item>
+              </SubMenu>
             </Menu>
           </Sider>
           <Layout>
@@ -168,7 +134,7 @@ export default class init_page extends Component {
                 className="site-layout-background"
                 style={{ padding: 24, minHeight: 360 }}
               >
-                <Route path='/init/director' component={director} />
+                <Route path="/init/director" component={director} />
               </div>
             </Content>
             <Footer style={{ textAlign: "center" }}>
