2f61084fadf88141851cfca0bd0dd5364c716cc3
Echochuan <912297261@qq.com>
2021-04-11 21:22:22 +0800

    获取表单中的值

--- src/pages/mark/markForDi/index.js
+++ src/pages/mark/markForDi/index.js
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
-import './index.css'
+import React from "react";
+import "./index.css";
 import { Form, Input, Button } from "antd";
 
-export default class index extends Component {
-  render() {
+const Demo = () => {
+
+    const onFinish = values => {
+      console.log(values);
+    };
 
     const buttonItemLayout = {
       wrapperCol: {
@@ -13,33 +16,38 @@ export default class index extends Component {
     };
 
     return (
-      <div>
+        <div>
           <div className="markbox">
-        <Form className="markform">
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item label="Question">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item {...buttonItemLayout}>
-            <Button type="primary">Submit</Button>
-          </Form.Item>
-        </Form>
+            <Form
+              className="markform"
+              onFinish={onFinish}
+            //   from={from}
+            >
+              <Form.Item label="Question" name="Question"  rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item label="Question" name="Question2" rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item label="Question" name="Question3" rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item label="Question" name="Question4" rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item label="Question" name="Question5" rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item label="Question" name="Question6" rules={[{ required: true }]}>
+                <Input placeholder="input placeholder" />
+              </Form.Item>
+              <Form.Item {...buttonItemLayout}>
+                <Button type="primary" htmlType="submit">Submit</Button>
+              </Form.Item>
+            </Form>
+          </div>
         </div>
-      </div>
-    );
-  }
-}
+      );
+    }
+
+    export default Demo;
