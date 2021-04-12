7c100fea449c0d2937460c9bbbf651ea96aa9d7b
Echochuan <912297261@qq.com>
2021-04-12 22:27:19 +0800

    提交之后生成一个包含职位、组名与各项分数的对象

--- src/pages/mark/markForDi/index.js
+++ src/pages/mark/markForDi/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import { Form, Input, Button, notification } from "antd";
 
-const Demo = () => {
+const Demo = (props) => {
 
     const openNotification = () => {
         notification.open({
@@ -15,10 +15,24 @@ const Demo = () => {
         });
       };
     
-        const onFinish = values => {
-          console.log(values);
-          openNotification();
-        };
+      const onFinish = (values) => {
+        const oldpath = props.location.pathname;
+        const newpathArr = oldpath.split('/');
+        const position = newpathArr[2];
+        const groupIndex = newpathArr[3];
+        // console.log(position);
+        // console.log(groupIndex);
+        console.log(values);
+        //要不要把username也存进路由里面？这样的话就不用redux管理了，username也可以从路由里面拿
+        const keyP = "position";
+        const keyG = "group";
+        const keyN = "username";
+        values[keyP] = position;
+        values[keyG] = groupIndex;
+        //把username取出来取代这个字符串
+        values[keyN] = "huangchenruo";
+        openNotification();
+      };
 
     const buttonItemLayout = {
       wrapperCol: {
