9b1ddd7b36a60c494a8ce0dc136a92cab982a015
Echochuan <912297261@qq.com>
2021-04-13 14:16:03 +0800

    将分数转换为数字类型

--- src/pages/mark/markForBe/index.js
+++ src/pages/mark/markForBe/index.js
@@ -2,78 +2,122 @@ import React from "react";
 import "./index.css";
 import { Form, Input, Button, notification } from "antd";
 
-const Demo = (props) => {
-
+const Demo = props => {
   const openNotification = () => {
     notification.open({
-      message: '评分成功',
-      description:
-        '你已完成该组的评分，请在左侧选择为其他组评分。',
+      message: "评分成功",
+      description: "你已完成该组的评分，请在左侧选择为其他组评分。",
       onClick: () => {
-        console.log('Notification Clicked!');
-      },
+        console.log("Notification Clicked!");
+      }
     });
   };
 
-    const onFinish = (values) => {
-      const oldpath = props.location.pathname;
-      const newpathArr = oldpath.split('/');
-      const position = newpathArr[2];
-      const groupIndex = newpathArr[3];
-      // console.log(position);
-      // console.log(groupIndex);
-      console.log(values);
-      //要不要把username也存进路由里面？这样的话就不用redux管理了，username也可以从路由里面拿
-      const keyP = "position";
-      const keyG = "group";
-      const keyN = "username";
-      values[keyP] = position;
-      values[keyG] = groupIndex;
-      //把username取出来取代这个字符串
-      values[keyN] = "huangchenruo";
-      openNotification();
-    };
-
-    const buttonItemLayout = {
-      wrapperCol: {
-        span: 14,
-        offset: 4
+  const toNumberValue = obj => {
+    if (obj instanceof Array) {
+      var arr = [];
+      for (var i = 0; i < obj.length; i++) {
+        arr[i] = Number(obj[i]);
       }
-    };
-
-    return (
-        <div>
-          <div className="markbox">
-            <Form
-              className="markform"
-              onFinish={onFinish}
-            //   from={from}
-            >
-              <Form.Item label="Question" name="Question"  rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="Question" name="Question2" rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="Question" name="Question3" rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="Question" name="Question4" rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="Question" name="Question5" rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item label="Question" name="Question6" rules={[{ required: true }]}>
-                <Input placeholder="input placeholder" />
-              </Form.Item>
-              <Form.Item {...buttonItemLayout}>
-                <Button type="primary" htmlType="submit">Submit</Button>
-              </Form.Item>
-            </Form>
-          </div>
-        </div>
-      );
+      return arr;
+    } else if (typeof obj == "object") {
+      for (var p in obj) {
+        obj[p] = Number(obj[p]);
+      }
+    } else if (typeof obj == "string") {
+      obj = Number(obj);
     }
+    return obj;
+  };
 
-    export default Demo;
+  const onFinish = values => {
+    const oldpath = props.location.pathname;
+    const newpathArr = oldpath.split("/");
+    const position = newpathArr[2];
+    const groupIndex = newpathArr[3];
+    const newValues = toNumberValue(values);
+    // console.log(position);
+    // console.log(groupIndex);
+    // console.log(values);
+    //要不要把username也存进路由里面？这样的话就不用redux管理了，username也可以从路由里面拿
+    const keyP = "position";
+    const keyG = "group";
+    const keyN = "username";
+    newValues[keyP] = position;
+    newValues[keyG] = groupIndex;
+    //把username取出来取代这个字符串
+    newValues[keyN] = "huangchenruo";
+
+    console.log(newValues);
+    openNotification();
+  };
+
+  const buttonItemLayout = {
+    wrapperCol: {
+      span: 14,
+      offset: 4
+    }
+  };
+
+  return (
+    <div>
+      <div className="markbox">
+        <Form
+          className="markform"
+          onFinish={onFinish}
+          //   from={from}
+        >
+          <Form.Item
+            label="Question"
+            name="Question"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item
+            label="Question"
+            name="Question2"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item
+            label="Question"
+            name="Question3"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item
+            label="Question"
+            name="Question4"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item
+            label="Question"
+            name="Question5"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item
+            label="Question"
+            name="Question6"
+            rules={[{ required: true }]}
+          >
+            <Input placeholder="input placeholder" />
+          </Form.Item>
+          <Form.Item {...buttonItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
--- src/pages/mark/markForFe/index.js
+++ src/pages/mark/markForFe/index.js
@@ -15,22 +15,42 @@ const Demo = (props) => {
     });
   };
 
-  const onFinish = (values) => {
+  const toNumberValue = obj => {
+    if (obj instanceof Array) {
+      var arr = [];
+      for (var i = 0; i < obj.length; i++) {
+        arr[i] = Number(obj[i]);
+      }
+      return arr;
+    } else if (typeof obj == "object") {
+      for (var p in obj) {
+        obj[p] = Number(obj[p]);
+      }
+    } else if (typeof obj == "string") {
+      obj = Number(obj);
+    }
+    return obj;
+  };
+
+  const onFinish = values => {
     const oldpath = props.location.pathname;
-    const newpathArr = oldpath.split('/');
+    const newpathArr = oldpath.split("/");
     const position = newpathArr[2];
     const groupIndex = newpathArr[3];
+    const newValues = toNumberValue(values);
     // console.log(position);
     // console.log(groupIndex);
-    console.log(values);
+    // console.log(values);
     //要不要把username也存进路由里面？这样的话就不用redux管理了，username也可以从路由里面拿
     const keyP = "position";
     const keyG = "group";
     const keyN = "username";
-    values[keyP] = position;
-    values[keyG] = groupIndex;
+    newValues[keyP] = position;
+    newValues[keyG] = groupIndex;
     //把username取出来取代这个字符串
-    values[keyN] = "huangchenruo";
+    newValues[keyN] = "huangchenruo";
+
+    console.log(newValues);
     openNotification();
   };
 
