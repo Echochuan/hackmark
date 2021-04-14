d72e90900084de397ed87f1c3093d88831273608
Echochuan <912297261@qq.com>
2021-04-15 00:07:10 +0800

    解决数值报错区间均为0-10的问题

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,7 @@ const defaultState = {
     username: ''
 }
 
+// eslint-disable-next-line
 export default (state = defaultState, action) => {
     console.log('redux is on');
     return action['type'] === 'get_name' ? Object.assign({}, state, action) : state;
